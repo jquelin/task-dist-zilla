86652e812ef12f0560c391854077f6681bf1c4a2
Jérôme Quelin <jquelin@gmail.com>
2010-01-07 13:13:38 +0100

    stub for task

---
+++ lib/Task/Dist/Zilla.pm
@@ -0,0 +1,62 @@
+use 5.008;
+use strict;
+use warnings;
+
+package Task::Dist::Zilla;
+# ABSTRACT: task to install dist-zilla and all its plugins
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Task::Dist::Zilla'
+
+=head1 DESCRIPTION
+
+This task is merely a placeholder to pull all dist-zilla related modules
+in one go.
+
+
+=pkgroup Base application
+
+=pkg Dist::Zilla
+
+=pkgroup Plugins
+
+=pkgroup Plugin bundles
+
+
+=head1 SEE ALSO
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-Dist-Zilla>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Dist-Zilla>
+
+=item * Mailing-list (same as dist-zilla)
+
+L<http://www.listbox.com/subscribe/?list_id=139292>
+
+=item * Git repository
+
+L<http://github.com/jquelin/task-dist-zilla.git>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-Dist-Zilla>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-Dist-Zilla>
+
+=back
+
+
